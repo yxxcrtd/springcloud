bcfb131d78d5d9a60150383022b6e975ee90ae9d
yxx <272795968@qq.com>
2022-02-16 11:08:24 +0800

    no message

--- microservice-user-consumer/src/main/java/com/example/controller/TestController.java
+++ microservice-user-consumer/src/main/java/com/example/controller/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("manage/test")
+@RequestMapping("")
 public class TestController {
 
     private Logger logger = LogManager.getLogger(TestController.class);
